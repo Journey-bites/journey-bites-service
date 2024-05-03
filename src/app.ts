6e10f8cd4411cf3c3b072200290ac53de771671e
enzo.luo <duenzo1010@gmail.com>
2024-05-03 11:49:53 +0800

    chore: typescript setup

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import express, { Request, Response, NextFunction } from "express";
+
+const PORT = process.env.PORT ?? 3001;
+
+const app = express();
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () =>
+  console.log(`Server is running on http://localhost:${PORT}`)
+);
+
+export default app;

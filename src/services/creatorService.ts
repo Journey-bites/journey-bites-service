64f0e6b5decbe61b0aeb43d5e244ec1be2171ac0
enzo.luo <duenzo1010@gmail.com>
2024-06-13 02:32:04 +0800

    feat: random creators

--- src/services/creatorService.ts
+++ src/services/creatorService.ts
@@ -8,8 +8,56 @@ type getCreatorsPayload = {
   searchName?: string;
 };
 
+const generateRandomOrder = () => (Math.random() > 0.5 ? 'asc' : 'desc');
+
+const generateRandomOrderCondition = () => {
+  const randomOrder = Math.floor(Math.random() * 5);
+  switch (randomOrder) {
+    case 0:
+      return {
+        profile: {
+          avatarImageUrl: generateRandomOrder(),
+        },
+      };
+    case 1:
+      return {
+        profile: {
+          socialLinks: {
+            website: generateRandomOrder(),
+          },
+        },
+      };
+    case 2:
+      return {
+        profile: {
+          socialLinks: {
+            instagram: generateRandomOrder(),
+          },
+        },
+      };
+    case 3:
+      return {
+        profile: {
+          socialLinks: {
+            facebook: generateRandomOrder(),
+          },
+        },
+      };
+    case 4:
+      return {
+        updatedAt: generateRandomOrder(),
+      };
+    default:
+      return {
+        id: generateRandomOrder(),
+      };
+  }
+};
+
 const getCreators = async ({ page = 1, pageSize = 10, type = 'common', searchName = '' }: getCreatorsPayload) => {
   let orderBy: any = {};
+
+  const randomOrderBy = generateRandomOrderCondition();
 
   switch (type) {
     case 'hot':
@@ -20,7 +68,9 @@ const getCreators = async ({ page = 1, pageSize = 10, type = 'common', searchNam
       };
       break;
     case 'random':
-      // TODO: Implement random order
+      orderBy = {
+        ...randomOrderBy,
+      };
       break;
     default:
       orderBy = {
@@ -29,56 +79,60 @@ const getCreators = async ({ page = 1, pageSize = 10, type = 'common', searchNam
       break;
   }
 
-  const creatorsDetails = await db.user.findMany({
-    where: {
-      profile: {
-        displayName: {
-          contains: searchName,
-          mode: 'insensitive',
-        },
-      },
-    },
-    select: {
-      id: true,
-      email: true,
-      profile: {
-        select: {
-          avatarImageUrl: true,
-          displayName: true,
-          socialLinks: {
-            select: {
-              website: true,
-              instagram: true,
-              facebook: true,
-            },
+  try {
+    const creatorsDetails = await db.user.findMany({
+      where: {
+        profile: {
+          displayName: {
+            contains: searchName,
+            mode: 'insensitive',
           },
         },
       },
-      follows: {
-        select: {
-          followingId: true,
+      select: {
+        id: true,
+        email: true,
+        profile: {
+          select: {
+            avatarImageUrl: true,
+            displayName: true,
+            socialLinks: {
+              select: {
+                website: true,
+                instagram: true,
+                facebook: true,
+              },
+            },
+          },
+        },
+        follows: {
+          select: {
+            followingId: true,
+          },
+        },
+        followedBy: {
+          select: {
+            followerId: true,
+          },
         },
       },
-      followedBy: {
-        select: {
-          followerId: true,
-        },
-      },
-    },
-    orderBy,
-    skip: (page - 1) * pageSize,
-    take: pageSize,
-  });
+      orderBy,
+      skip: (page - 1) * pageSize,
+      take: pageSize,
+    });
 
-  const creators = creatorsDetails.map((creator) => ({
-    userId: creator.id,
-    email: creator.email,
-    ...creator.profile,
-    followers: creator.follows,
-    followings: creator.followedBy,
-  }));
+    const creators = creatorsDetails.map((creator) => ({
+      userId: creator.id,
+      email: creator.email,
+      ...creator.profile,
+      followers: creator.follows,
+      followings: creator.followedBy,
+    }));
 
-  return creators;
+    return creators;
+  } catch (error) {
+    throw new Error('Error while getting creators');
+  }
 };
 
 export default {

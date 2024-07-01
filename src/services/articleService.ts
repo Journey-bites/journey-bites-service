a5ad66f00c6004ab8f7552ad83fd3e354c2181e7
jweeei <aaa51782+wei@gmail.com>
2024-07-02 00:32:18 +0800

    feat: add authorship verification

--- src/services/articleService.ts
+++ src/services/articleService.ts
@@ -167,7 +167,7 @@ const createArticle = async (creatorId: string, payload: CreateArticlePayload) =
   }
 };
 
-const getArticleById = async (articleId: string) => {
+const getArticleById = async (articleId: string, userId?: string) => {
   try {
     const article = await db.article.findUnique({
       where: {
@@ -230,15 +230,13 @@ const getArticleById = async (articleId: string) => {
       },
     });
 
-    if (!article?.isNeedPay) {
-      return article;
-    }
+    if (article?.creator?.id === userId) return article;
+
+    if (!article?.isNeedPay) return article;
 
     const content = truncate(article?.content, { length: 100, stripTags: false });
 
     return { ...article, content };
-
-    // return article;
   } catch (error) {
     throw new Error('Error while getting article');
   }

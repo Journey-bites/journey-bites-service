160485f21ae7dc892660738390cfc2719cb2a230
jweeei <aaa51782+wei@gmail.com>
2024-07-02 14:30:07 +0800

    refactor: move server logic to controller

--- src/services/articleService.ts
+++ src/services/articleService.ts
@@ -2,7 +2,6 @@ import db from '@/db';
 import { Prisma } from '@prisma/client';
 
 import { PrismaClientErrorCode } from '@/types/PrismaClientErrorCode';
-import truncate from 'truncate-html';
 
 type GetArticlesPayload = {
   page?: number;
@@ -199,7 +198,7 @@ const createArticle = async (creatorId: string, payload: CreateArticlePayload) =
   }
 };
 
-const getArticleById = async (articleId: string, userId?: string) => {
+const getArticleById = async (articleId: string) => {
   try {
     const article = await db.article.findUnique({
       where: {
@@ -262,13 +261,7 @@ const getArticleById = async (articleId: string, userId?: string) => {
       },
     });
 
-    if (article?.creator?.id === userId) return article;
-
-    if (!article?.isNeedPay) return article;
-
-    const content = truncate(article?.content, { length: 100, stripTags: false });
-
-    return { ...article, content };
+    return article;
   } catch (error) {
     throw new Error('Error while getting article');
   }
